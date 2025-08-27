4e4a5796b5bcf6ce3b3bb1cc4a0d55ac8a4decc1
MasayukiTamate <masayuki.tamate.anago@gmail.com>
2025-08-27 13:57:34 +0900

    センテンス.py実装（仮１

--- sentiment.py
+++ sentiment.py
@@ -5,8 +5,25 @@ SENTIMENTS = "喜び、悲しみ、期待、驚き、怒り、恐れ、嫌悪、
 
 
 def get_model(model=MODEL):
-    pipe = pipeline("sentiment-analysis", model=model)
+    pipe = pippeline("sentiment-analysis", model=model)
     return pipe
 
 
-def get_sentiment(pipe, sen)
+def get_sentiment(pipe, sentence):
+    results = pipe(sentence)
+    label_number = int(results[0]["label"][-1:])
+    return SENTIMENTS[label_number]
+
+
+
+if __name__ == "__main__":
+    import sys
+    from aozora import get_aozora, parse_text_into_sentences
+
+    url = sys.argv[1]
+    text = get_aozora(url);
+    pipe = get_model();
+    sentences = parse_text_into_sentences(text)
+    emotions = [get_sentiment(pipe, sentence) for sentence in sentences]
+    stats = {e: emotions.count(e) for e in SENTIMENTS}
+    print(stats)
